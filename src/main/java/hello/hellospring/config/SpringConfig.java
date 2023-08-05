18ec3ecb5f71467c873883edcb6bdd8e130455f0
jaehee <jaeheepark0145@gmail.com>
2023-08-05 14:23:30 +0900

    feat: JpaRepository 추가

--- src/main/java/hello/hellospring/config/SpringConfig.java
+++ src/main/java/hello/hellospring/config/SpringConfig.java
@@ -11,20 +11,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
-
-    @Bean
-    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-        return new JPaMemberRepository(em);
+        return new MemberService(memberRepository);
     }
 }
