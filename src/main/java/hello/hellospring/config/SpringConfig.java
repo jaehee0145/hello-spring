4e9166073945c4a745eac703b847be66bbc477fd
jaehee <jaeheepark0145@gmail.com>
2023-08-05 14:12:50 +0900

    feat: JPA 적용

--- src/main/java/hello/hellospring/config/SpringConfig.java
+++ src/main/java/hello/hellospring/config/SpringConfig.java
@@ -1,13 +1,21 @@
 package hello.hellospring.config;
 
+import hello.hellospring.repository.JPaMemberRepository;
 import hello.hellospring.service.MemberService;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import jakarta.persistence.EntityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
+
+    private EntityManager em;
+
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +24,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JPaMemberRepository(em);
     }
 }
